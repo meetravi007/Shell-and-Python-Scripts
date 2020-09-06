251a7e3c4e7bb6bad68cf722fcc5e550a2074dd7
Ravindra Bhargava <32759786+ravindrabhargava@users.noreply.github.com>
2020-09-06 17:06:34 +0800

    Update automate-k8s-shell.sh

--- automate-k8s-shell.sh
+++ automate-k8s-shell.sh
@@ -1,1 +1,45 @@
+#!/bin/sh
 
+# Author        : Ravindra Bhargava
+# Email         : meetravi007@gmail.com
+# Description   : Jenkins deploy script using docker hub on Kubernetes
+# website       : https://ravindrabhargava.com
+# Documentation : https://www.terraform.io/docs/commands/apply.html
+
+echo -n "Enter the cluster name: "
+read cname
+echo -n "Enter the region name: "
+read rname
+
+gcloud container clusters get-credentials $cname --region $rname &&
+
+echo "\033[1;35;40mcluster credentials has been generated for $cname at region $rname.!!\033[0m"
+
+
+read -p "Deploying jenkins app on kubernetes cluster, please enter to confirm!!" continue
+
+kubectl apply --filename hello-k8s-deploy.yaml &&
+
+echo "\033[1;35;40mJenkins using docker hub image have been deployed into kubernetes cluster $cname at region $rname.!!\033[0m"
+
+read -p "Creating internal load balancer for all jenkins instances,please enter to confirm..." continue 
+			
+kubectl apply --filename hello-k8s-svc.yaml &&
+
+echo "\033[1;35;40mNetwork service has been deployed into your kubernetes cluster $cname at region $rname.!!\033[0m"
+
+kubectl get service &&
+
+echo "\033[1;35;40mNetwork service has been deployed and genretae the end point external ip of jenkins kubernetes cluster servers $cname at region $rname.!!\033[0m"
+
+sleep 7
+
+kubectl get service
+
+echo "\033[1;35;40mPlease note down the external ip and use it in browser to acccess the jenkins url http://EXTERNAL-IP:8080 \033[0m"
+
+sleep 5 
+
+kubectl get service
+
+echo "\033[1;35;40mRun the command <kubectl get service> again in case external ip still pending\033[0m"
